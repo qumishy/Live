2bf5e3f8d8cc06c86ac300a0c51779ec2b9a1dc1
qumishy <qumishy@gmail.com>
2026-05-23 19:46:23 +0300

    fix m3u parser

--- LiveApp/server.js
+++ LiveApp/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 
-const PLAYLIST_URL =
-  "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8";
+const PLAYLIST_URL = "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8";
 
 function parseM3U(text) {
-  const lines = text.split("\n");
+  const lines = text.split(/\r?\n/);
   const channels = [];
 
   for (let i = 0; i < lines.length; i++) {
@@ -13,9 +12,22 @@ function parseM3U(text) {
 
     if (line.startsWith("#EXTINF")) {
       const name = line.split(",").pop().trim();
-      const url = lines[i + 1]?.trim();
 
-      if (url && url.startsWith("http")) {
+      let url = "";
+      for (let j = i + 1; j < lines.length; j++) {
+        const next = lines[j].trim();
+
+        if (!next) continue;
+        if (next.startsWith("#EXTINF")) break;
+        if (next.startsWith("#")) continue;
+
+        if (next.startsWith("http")) {
+          url = next;
+          break;
+        }
+      }
+
+      if (url) {
         channels.push({
           id: channels.length + 1,
           name,
@@ -32,7 +44,7 @@ app.get("/", (req, res) => {
   res.json({
     status: "online",
     message: "Live API is running",
-    endpoints: ["/channels"]
+    source: PLAYLIST_URL
   });
 });
 
